1680dd9ed506a40258c80c9f2291b19bf919fee0
PaulVen <paul.venckunas@gmail.com>
2025-06-28 22:43:53 +0200

    third commit

--- lambda/main.py
+++ lambda/main.py
@@ -2,7 +2,7 @@ def handler(event, context):
     print(event)
     response_body = {
         "message": "Hello World",
-        "version": "1.0.0"
+        "version": "1.0.1"
     }
     return {
         "statusCode": 200,
