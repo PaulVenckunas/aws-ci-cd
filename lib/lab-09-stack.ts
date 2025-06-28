826ad873b4e8234f5cc89fbd24e3b0e9ae1f8c9b
PaulVen <paul.venckunas@gmail.com>
2025-06-28 22:10:24 +0200

    second commit

---
+++ lib/lab-09-stack.ts
@@ -0,0 +1,35 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class Lab09Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaFunction = new lambda.Function(this, "LambdaFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler"
+    });
+
+    const functionUrl = lambdaFunction.addFunctionUrl({
+      // url that we can call from anywhere
+      // no authentication - publicly accessable
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"]
+      }
+    });
+
+    new cdk.CfnOutput(this, "Url", {
+      value: functionUrl.url
+    });
+  }
+}
+
+// Bootstrap
+// $ cdk bootstrap --region us-east-1
+// Deploy
+// $ cdk deploy
---
+++ test/lab-09.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Lab09 from '../lib/lab-09-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lab-09-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Lab09.Lab09Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
