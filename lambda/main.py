12b8056007829386ab453b95a515b998f99225d7
PaulVen <paul.venckunas@gmail.com>
2025-06-29 22:20:01 +0200

    sdsfd

--- lambda/main.py
+++ lambda/main.py
@@ -1,23 +1,27 @@
 import os
-import json
+# boto3 is aws-sdk for python
 import boto3
 
 
 def handler(event, context):
-    # Raw event data
-    path = event.get("rawPath")
+
+    # Raw event data.
+    path = event["rawPath"]
     if path != "/":
         return {"statusCode": 404, "body": "Not found."}
 
-    # Get a reference to the DynamoDB table
+    # get a reference to the DDB table
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(os.environ.get("TABLE_NAME"))
 
-    # Read the "visit_count" key (or initialize it to 0)
+    # Read the "VISIT COUNT" key (or create it if it doesnt exist)
     response = table.get_item(Key={"key": "visit_count"})
-    visit_count = response.get("Item", {}).get("value", 0)
+    if "Item" in response:
+        visit_count = response["Item"]["value"]
+    else:
+        visit_count = 0
 
-    # Increment the visit count and write it back to the table
+    #Increment the visit count and write it back to the table.
     new_visit_count = 1
     table.put_item(Item={"key": "visit_count", "value": new_visit_count})
 
@@ -27,9 +31,7 @@ def handler(event, context):
         "version": version,
         "visit_count": new_visit_count
     }
-
     return {
         "statusCode": 200,
-        "body": json.dumps(response_body),
-        "headers": {"Content-Type": "application/json"}
+        "body": response_body
     }
