94523d92b14fff925300b17de24ea7a35ec348ce
PaulVen <paul.venckunas@gmail.com>
2025-06-29 22:08:35 +0200

    update lambda

--- lambda/main.py
+++ lambda/main.py
@@ -1,27 +1,23 @@
 import os
-# boto3 is aws-sdk for python
+import json
 import boto3
 
 
 def handler(event, context):
-
-    # Raw event data.
-    path = event["rawPath"]
+    # Raw event data
+    path = event.get("rawPath")
     if path != "/":
         return {"statusCode": 404, "body": "Not found."}
 
-    # get a reference to the DDB table
+    # Get a reference to the DynamoDB table
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(os.environ.get("TABLE_NAME"))
 
-    # Read the "VISIT COUNT" key (or create it if it doesnt exist)
+    # Read the "visit_count" key (or initialize it to 0)
     response = table.get_item(Key={"key": "visit_count"})
-    if "Item" in response:
-        visit_count = response["item"]["value"]
-    else:
-        visit_count = 0
+    visit_count = response.get("Item", {}).get("value", 0)
 
-    #Increment the visit count and write it back to the table.
+    # Increment the visit count and write it back to the table
     new_visit_count = visit_count + 1
     table.put_item(Item={"key": "visit_count", "value": new_visit_count})
 
@@ -31,7 +27,9 @@ def handler(event, context):
         "version": version,
         "visit_count": new_visit_count
     }
+
     return {
         "statusCode": 200,
-        "body": response_body
+        "body": json.dumps(response_body),
+        "headers": {"Content-Type": "application/json"}
     }
